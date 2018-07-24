c447fb60369e66b18bdfa479402261adafb39d23
yuliang <2249805005@qq.com>
2018-07-24 18:29:22 +0800

    添加关注和粉丝

--- app/src/main/java/com/renyi/maxsin/module/mvp/MvpFragment.java
+++ app/src/main/java/com/renyi/maxsin/module/mvp/MvpFragment.java
@@ -1,13 +1,37 @@
 package com.renyi.maxsin.module.mvp;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
 import com.renyi.maxsin.R;
 import com.renyi.maxsin.base.Basefragment;
+import com.renyi.maxsin.module.mvp.bean.MvpRecommendBean;
+import com.renyi.maxsin.net.Api;
+import com.renyi.maxsin.net.BaseCallback;
+import com.renyi.maxsin.net.OkHttpHelper;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
 
 /**
  * Created by zhangyuliang on 2018/3/22.
  */
 
-public class MvpFragment extends Basefragment {
+public class MvpFragment extends Basefragment implements ViewPager.OnPageChangeListener {
+
+    @BindView(R.id.viewpager)
+    ViewPager viewpager;
+    @BindView(R.id.viewPagerContainer)
+    RelativeLayout viewPagerContainer;
+    MyPagerAdapter adapter;
 
     @Override
     protected int getLayoutId() {
@@ -16,6 +40,22 @@ public class MvpFragment extends Basefragment {
 
     @Override
     protected void initView() {
+        viewpager.setClipChildren(false);
+        //父容器一定要设置这个，否则看不出效果
+        viewPagerContainer.setClipChildren(false);
+
+        adapter = new MyPagerAdapter();
+        if (viewpager != null) {
+            viewpager.setAdapter(adapter);
+        }
+
+        //设置ViewPager切换效果，即实现画廊效果
+        viewpager.setPageTransformer(true, new ZoomOutPageTransformer());
+        //设置预加载数量
+        viewpager.setOffscreenPageLimit(2);
+        //设置每页之间的左右间隔
+        viewpager.addOnPageChangeListener(this);
+        loadDataFromSer();
 
     }
 
@@ -28,4 +68,149 @@ public class MvpFragment extends Basefragment {
     protected void setOnclickListeners() {
 
     }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    class MyPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            //return list == null ? 0 : list.size();
+            return 10;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return (view == object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, final int position) {
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.item_get_act_list, null);
+            //            TextView time = (TextView) view.findViewById(R.id.time);
+            //            TextView info = (TextView) view.findViewById(R.id.info);
+            //            TextView teacher = (TextView) view.findViewById(R.id.teacher);
+            //            TextView tv04 = (TextView) view.findViewById(R.id.tv04);
+            //            RelativeLayout tv = (RelativeLayout) view.findViewById(R.id.rel03);
+            //
+            //            time.setText(list.get(position).getEvaluat_time());
+            //            info.setText(list.get(position).getEvaluat_result());
+            //            teacher.setText(list.get(position).getEvaluat_teacher());
+
+
+            //            view.setOnClickListener(new View.OnClickListener() {
+            //                @Override
+            //                public void onClick(View v) {
+            //                    //
+            //                    //                    Intent intent = new Intent(MyAssessActivity.this,
+            //                    //                            ProjectAssessDetailsActivity.class);
+            //                    //                    intent.putExtras(bundle);
+            //                    //                    startActivity(intent);
+            //                }
+            //            });
+            //
+                    ((ViewPager) container).addView(view);
+            return view;
+            //
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            ((ViewPager) container).removeView((View) object);
+        }
+    }
+
+
+    class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MAX_SCALE = 1f;
+        private static final float MIN_SCALE = 0.8f;//0.85f
+
+        @Override
+        public void transformPage(View view, float position) {
+            //setScaleY只支持api11以上
+            if (position < -1) {
+                view.setScaleX(MIN_SCALE);
+                view.setScaleY(MIN_SCALE);
+            } else if (position <= 1) //a页滑动至b页 ； a页从 0.0 -1 ；b页从1 ~ 0.0
+            { // [-1,1]
+                //              Log.e("TAG", view + " , " + position + "");
+                float scaleFactor = MIN_SCALE + (1 - Math.abs(position)) * (MAX_SCALE - MIN_SCALE);
+                view.setScaleX(scaleFactor);
+                //每次滑动后进行微小的移动目的是为了防止在三星的某些手机上出现两边的页面为显示的情况
+                if (position > 0) {
+                    view.setTranslationX(-scaleFactor * 2);
+                } else if (position < 0) {
+                    view.setTranslationX(scaleFactor * 2);
+                }
+                view.setScaleY(scaleFactor);
+
+            } else { // (1,+Infinity]
+
+                view.setScaleX(MIN_SCALE);
+                view.setScaleY(MIN_SCALE);
+
+            }
+        }
+
+    }
+
+
+    private void loadDataFromSer() {
+
+
+        OkHttpHelper mHttpHelper = OkHttpHelper.getinstance();
+        Map<String, String> map = new HashMap<>();
+        map.put("key", Api.KEY);
+        map.put("u_id", "1");
+
+        mHttpHelper.post(Api.URL + "tuijian", map, new BaseCallback<MvpRecommendBean>() {
+            @Override
+            public void onRequestBefore() {
+
+            }
+
+            @Override
+            public void onFailure(Request request, Exception e) {
+
+            }
+
+            @Override
+            public void onSuccess(Response response, MvpRecommendBean resultBean) {
+
+                if (resultBean.getCode().equals("800")) {
+                    //                    resultBeanData = resultBean.getData();
+                    //                    get_list = resultBean.getData().getGet_list();
+                    //                    get_listAll.addAll(get_list);
+                    //                    if (get_listAll.size() != 0) {
+                    //                        adapter.notifyDataSetChanged();
+                    //                        showEmpty(false);
+                    //                    } else {
+                    //                        showEmpty(true);
+                    //                    }
+                } else {
+
+                }
+
+            }
+
+            @Override
+            public void onError(Response response, int errorCode, Exception e) {
+
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/renyi/maxsin/module/mvp/bean/MvpRecommendBean.java
@@ -0,0 +1,99 @@
+package com.renyi.maxsin.module.mvp.bean;
+
+import java.util.List;
+
+/**
+ * Created by zhangyuliang on 2018/7/24.
+ */
+
+public class MvpRecommendBean {
+    /**
+     * code : 800
+     * message : 成功
+     * data : [{"id":"12","nickname":"lei","head_url":"","cover_img":"http://renyi.mxsyzen.com/publish_imgs/1531902163764942_cover_.jpg","is_focus":"1"},{"id":"133","nickname":"","head_url":"","cover_img":"http://renyi.mxsyzen.com/publish_imgs/1531902163764942_cover_.jpg","is_focus":"1"}]
+     */
+
+    private String code;
+    private String message;
+    private List<DataBean> data;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<DataBean> getData() {
+        return data;
+    }
+
+    public void setData(List<DataBean> data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * id : 12
+         * nickname : lei
+         * head_url :
+         * cover_img : http://renyi.mxsyzen.com/publish_imgs/1531902163764942_cover_.jpg
+         * is_focus : 1
+         */
+
+        private String id;
+        private String nickname;
+        private String head_url;
+        private String cover_img;
+        private String is_focus;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
+
+        public String getHead_url() {
+            return head_url;
+        }
+
+        public void setHead_url(String head_url) {
+            this.head_url = head_url;
+        }
+
+        public String getCover_img() {
+            return cover_img;
+        }
+
+        public void setCover_img(String cover_img) {
+            this.cover_img = cover_img;
+        }
+
+        public String getIs_focus() {
+            return is_focus;
+        }
+
+        public void setIs_focus(String is_focus) {
+            this.is_focus = is_focus;
+        }
+    }
+}
